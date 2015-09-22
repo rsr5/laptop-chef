1f73389508540f633785dc28376540f9eb9bb2a7
Robin Ridler <robin.ridler@gmail.com>
2015-09-22 19:34:56 +0100

    Adding vim plugins

---
+++ cookbooks/robin-desktop/recipes/vim.rb
@@ -0,0 +1,18 @@
+
+package 'vim'
+
+plugins = {
+  'vim-python-pep8-indent' => 'https://github.com/hynek/vim-python-pep8-indent.git',
+  'python-mode' => 'https://github.com/klen/python-mode.git',
+  'vim-sensible' => 'git://github.com/tpope/vim-sensible.git',
+  'vim-airline' => 'https://github.com/bling/vim-airline.git',
+  'tmuxline' => 'https://github.com/edkolev/tmuxline.vim' 
+}
+
+plugins.each do |plugin, url|
+  git "/home/robin/.vim/bundle/#{plugin}" do
+    repository url
+    revision 'master'
+    action :sync
+  end
+end
