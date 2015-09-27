f524fa7dc46e4429295d62971e1f46afd0fc9c93
Robin Ridler <robin.ridler@datasift.com>
2015-09-27 15:22:27 +0100

    New names for the internal run_chef command

--- run_chef.sh
+++ run_chef.sh
@@ -2,7 +2,7 @@
 
 rm -rf berks-cookbooks
 
-berks vendor --berksfile cookbooks/Berksfile berks-cookbooks
+berks vendor --berksfile Berksfile berks-cookbooks
 
 sudo chef-client -c client.rb -j dna.json
 
