d5a09719bd1bc5a7ac3b8da5846548065499c49f
Robin Ridler <robin.ridler@gmail.com>
2015-10-11 11:53:31 +0100

    Setup chefdk on all hosts

--- cookbooks/base/recipes/packages.rb
+++ cookbooks/base/recipes/packages.rb
@@ -18,3 +18,16 @@ bash 'install compilers' do
     EOH
   not_if 'rpm -qa | grep gcc-c++'
 end
+
+package 'chefdk' do
+  source "#{Chef::Config['file_cache_path']}/chefdk.rpm"
+  action :nothing
+end
+
+remote_file "#{Chef::Config['file_cache_path']}/chefdk.rpm" do
+  source 'https://opscode-omnibus-packages.s3.amazonaws.com/el/7/x86_64/'\
+         'chefdk-0.7.0-1.el7.x86_64.rpm'
+  mode '0644'
+  action :create
+  notifies :install, 'package[chefdk]', :immediately
+end
--- cookbooks/workstation/recipes/setup_chef.rb
+++ cookbooks/workstation/recipes/setup_chef.rb
@@ -4,19 +4,6 @@ directory '/home/robin/code' do
   group 'robin'
 end
 
-package 'chefdk' do
-  source "#{Chef::Config['file_cache_path']}/chefdk.rpm"
-  action :nothing
-end
-
-remote_file "#{Chef::Config['file_cache_path']}/chefdk.rpm" do
-  source 'https://opscode-omnibus-packages.s3.amazonaws.com/el/7/x86_64/'\
-         'chefdk-0.7.0-1.el7.x86_64.rpm'
-  mode '0644'
-  action :create
-  notifies :install, 'package[chefdk]', :immediately
-end
-
 ruby_block 'add_to_path' do
   block do
     ENV['OLD_GEM_HOME'] = ENV['GEM_HOME']
