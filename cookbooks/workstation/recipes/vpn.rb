9362ad194527ff832172d571ee4f7a7b8cd74805
Robin Ridler <robin.ridler@gmail.com>
2015-10-13 09:49:48 +0100

    Adding script to connect to all vpns

--- cookbooks/workstation/recipes/vpn.rb
+++ cookbooks/workstation/recipes/vpn.rb
@@ -1,2 +1,14 @@
 
 package 'vpnc'
+package 'iptraf'
+
+file '/usr/local/bin/vpn' do
+  content <<-MOD
+#!/bin/bash
+
+sudo vpnc
+sudo vpnc --local-port 0 staging.conf
+sudo vpnc --local-port 0 prod.conf
+  MOD
+  mode '0755'
+end
