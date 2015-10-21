44e0f008f931e6051081f3dc4148f1c22222ec27
Robin Ridler <robin.ridler@gmail.com>
2015-10-21 13:57:57 +0100

    Adding mutate plugin

--- cookbooks/workstation/recipes/vagrant.rb
+++ cookbooks/workstation/recipes/vagrant.rb
@@ -1,7 +1,7 @@
 
 include_recipe 'vagrant::fedora'
 
-%w(vagrant-omnibus vagrant-berkshelf).each do |plugin|
+%w(vagrant-omnibus vagrant-berkshelf vagrant-mutate).each do |plugin|
   vagrant_plugin plugin do
     user 'robin'
   end 
