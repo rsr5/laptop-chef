ceb8b120d5bed49c12cccb312148e99443789791
Robin Ridler <robin.ridler@gmail.com>
2015-11-22 14:31:08 +0000

    Adding some vim plugins

--- cookbooks/base/recipes/vim.rb
+++ cookbooks/base/recipes/vim.rb
@@ -1,5 +1,6 @@
 
 package 'vim'
+package 'the_silver_searcher'
 
 plugins = {
   'vim-python-pep8-indent' => 'https://github.com/hynek/vim-python-pep8-indent.git',
@@ -10,7 +11,9 @@ plugins = {
   'vim-colors-solarized' => 'git://github.com/altercation/vim-colors-solarized.git',
   'syntastic' => 'https://github.com/scrooloose/syntastic.git',
   'ctrlp' => 'https://github.com/kien/ctrlp.vim.git',
-  'gitgutter' => 'git://github.com/airblade/vim-gitgutter.git'
+  'gitgutter' => 'git://github.com/airblade/vim-gitgutter.git',
+  'minibufferexplorer' => 'https://github.com/fholgado/minibufexpl.vim.git',
+  'surround' => 'https://github.com/tpope/vim-surround'
 }
 
 plugins.each do |plugin, url|
@@ -19,4 +22,13 @@ plugins.each do |plugin, url|
     revision 'master'
     action :sync
   end
+end
+
+# Workaround to ignore rubocop version warnings
+file '/usr/bin/chefrubocop' do
+  mode 0755
+  content <<-CONT
+#! /bin/sh
+exec chef exec rubocop "$@"
+  CONT
 end
