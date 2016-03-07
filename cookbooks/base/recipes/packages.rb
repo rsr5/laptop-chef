9ad633d3c53c4d86eb3d66e522cf157375a8a86f
Robin Ridler <robin.ridler@gmail.com>
2016-03-07 09:18:11 +0000

    Upgrade ChefDK

--- cookbooks/base/recipes/packages.rb
+++ cookbooks/base/recipes/packages.rb
@@ -26,7 +26,7 @@ end
 
 remote_file "#{Chef::Config['file_cache_path']}/chefdk.rpm" do
   source 'https://opscode-omnibus-packages.s3.amazonaws.com/el/7/x86_64/'\
-         'chefdk-0.9.0-1.el7.x86_64.rpm'
+         'chefdk-0.11.0-1.el7.x86_64.rpm'
   mode '0644'
   action :create
   notifies :upgrade, 'package[chefdk]', :immediately
