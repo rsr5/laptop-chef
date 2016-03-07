2ee1e00c5a0b2e5e426f1ed81e1385347c466f15
Robin Ridler <robin.ridler@gmail.com>
2016-03-07 09:20:39 +0000

    Recipe for install virtualbox

---
+++ cookbooks/workstation/recipes/virtualbox.rb
@@ -0,0 +1,33 @@
+
+yum_repository 'virtualbox' do
+  description 'Fedora $releasever - $basearch - VirtualBox'
+  baseurl 'http://download.virtualbox.org/'\
+          'virtualbox/rpm/fedora/$releasever/$basearch'
+  enabled true
+  gpgcheck true
+  repo_gpgcheck true
+  gpgkey 'https://www.virtualbox.org/download/oracle_vbox.asc'
+end
+
+package 'VirtualBox-5.0'
+
+execute 'setup vbox driver' do
+  command '/usr/lib/virtualbox/vboxdrv.sh'
+  action :nothing
+end
+
+package 'vbox dependencies' do
+  package_name %w(
+    binutils
+    gcc
+    make
+    patch
+    libgomp
+    glibc-headers
+    glibc-devel
+    kernel-headers
+    kernel-devel
+    dkms
+  )
+  notifies :run, 'execute[setup vbox driver]', :immediately
+end
